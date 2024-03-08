3147bf1ecdde4c0e08454c60e721344a12fd5a3c
cloner174 <hamedhajipour1400@gmail.com>
2024-03-08 03:32:49 +0330

    modified:   index.py

--- index.py
+++ index.py
@@ -1,6 +1,10 @@
 #
 from main import Light
 
+import matplotlib.pyplot as plt
+from matplotlib.ticker import FormatStrFormatter
+
+
 
 step = 0.011 #input("Please insert the Total number of STEPS, here -->>   ")
 first = 800  #input("Please insert the number for First step, here -->>   ")
@@ -14,19 +18,30 @@ reflectance_valuesR = []
 reflectance_valuesT = []
 
 lambda0 = 1600
+
 for d in range( total + 1):
     
     Landa_val = first + ( step*(d) )
     
     ReflectanceRT = Light(landa0= lambda0, landa= Landa_val)
     
-    ReflectanceR, ReflectanceT = ReflectanceRT.main()
+    ReflectanceR, ReflectanceT = ReflectanceRT.run()
     
     lambda_values.append(Landa_val)
     
-    reflectance_valuesR.append(ReflectanceR[1])
-    reflectance_valuesT.append(ReflectanceT[1])
+    reflectance_valuesR.append(ReflectanceR)
+    reflectance_valuesT.append(ReflectanceT)
     
-    
-    
-    
+
+print(len(reflectance_valuesR))
+print(len(reflectance_valuesT))
+print(len(lambda_values))
+
+#lambda_values.pop()
+
+fig, ax = plt.subplots()
+ax.yaxis.set_major_formatter(FormatStrFormatter('%.17f'))
+fig.set_figheight(5)
+fig.set_figwidth(15)
+plt.plot(lambda_values, reflectance_valuesR)
+plt.show()
